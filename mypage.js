139ef2230890a4985f1de02f4c4ca6dddbf293b7
Raghad-ibrahim-alshiban <153172255+Raghad-ibrahim-alshiban@users.noreply.github.com>
2024-07-06 18:12:50 +0300

    Add files via upload

---
+++ mypage.js
@@ -0,0 +1,18 @@
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Observation, Inference, Engineering specifications.";
+  document.getElementById("demo").style.color = "Chocolate";
+  document.getElementById("demo").style.fontSize = "25px";
+ 
+  
+}
+function myFunction2() {
+ document.getElementById("demo2").innerHTML = "Observation, Measurement, Prediction";
+  document.getElementById("demo2").style.color="Chocolate";
+  document.getElementById("demo2").style.fontSize = "25px";
+}
+function myFunction3() {
+ document.getElementById("demo1").innerHTML = "Observation, Inference, Aeronautics skills";
+  document.getElementById("demo1").style.color="Brown";
+  document.getElementById("demo1").style.fontSize = "25px";
+}
+
